c5e7651262320a820b12be2369301e5162b31bec
m1kke <koskela.m@gmail.com>
2015-02-14 18:43:06 +0200

    Added menuFunction.sh and updated main.sh to call it

---
+++ menuFunction.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+printMenu(){
+    clear
+    echo
+    echo "*************"
+    echo "Menu"
+    echo "*************"
+    echo "Choose what to do:"
+    echo "1. Install programs"
+    echo "2. Install configs"
+    echo "3. Exit"
+    echo
+}
